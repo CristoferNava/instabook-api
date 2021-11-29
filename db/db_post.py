bbfa3c7bebb32af0ab0e26078aa58cc9622d8546
CristoferNava <cj.carmonanava@ugto.mx>
2021-11-29 09:07:06 -0600

    delete post was added

--- db/db_post.py
+++ db/db_post.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from sqlalchemy.orm.session import Session
 from routers.schemas import PostBase
 from db.models import DbPost
@@ -20,3 +21,16 @@ def create_post(db: Session, request: PostBase):
 
 def get_all(db: Session):
     return db.query(DbPost).all()
+
+
+def delete_post(db: Session, id: int, user_id: int):
+    post = db.query(DbPost).filter(DbPost.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with {id} not found")
+    if post.user_id != user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Only post creator can delete the post")
+    db.delete(post)
+    db.commit()
+    return "ok"
